8e521266577ab850eddfdf72d7d79216f299ba3b
dweverson <dweverson@gmail.com>
2022-05-02 21:39:48 +0100

    primary tag alias association added, redundant tag find function removed

--- app/models/influencer.rb
+++ app/models/influencer.rb
@@ -2,12 +2,10 @@ class Influencer < ApplicationRecord
   belongs_to :platform
   has_many :influencer_tags
   has_many :tags, through: :influencer_tags
+  belongs_to :primary_tag, class_name: "Tag"
 
   validates :followers, :handle, presence: true
   validates :profile_pic_url, :url => true
   validates :platform_id, uniqueness: { scope: :handle }
 
-  def primary_tag
-    Tag.find(primary_tag_id)
-  end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,6 @@
 class Tag < ApplicationRecord
     has_many :influencer_tags
+    has_many :inf_primary_tags, class_name: "Influencer", foreign_key: :primary_tag_id
 
     validates :name, presence: true
 end
